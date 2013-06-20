791ed19fcd973118aad35fe82a177f0aaae5fe1f
develpr <kevin@develpr.com>
2013-06-19 23:55:36 -0700

    first commit

---
+++ Matchismo/Model/Card.h
@@ -0,0 +1,13 @@
+//
+//  Card.h
+//  Matchismo
+//
+//  Created by Kevin Mitchell on 6/19/13.
+//  Copyright (c) 2013 Kevin Mitchell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@end
---
+++ Matchismo/Model/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  Matchismo
+//
+//  Created by Kevin Mitchell on 6/19/13.
+//  Copyright (c) 2013 Kevin Mitchell. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
