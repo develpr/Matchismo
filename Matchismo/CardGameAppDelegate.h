c803ffbc474e89362c68a44e958e7ce614ece799
develpr <kevin@develpr.com>
2013-06-19 23:22:11 -0700

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Kevin Mitchell on 6/19/13.
+//  Copyright (c) 2013 Kevin Mitchell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
