7e39a719a61c3af35e6b1cf56079df9fce7a9181
develpr <kevin@develpr.com>
2013-07-02 23:31:43 -0700

    Pre Homework 1

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -7,22 +7,33 @@
 //
 
 #import "CardGameViewController.h"
-#import "PlayingCard.h"
+#import "PlayingCardDeck.h"
+#import "CardMatchingGame.h"
 
 @interface CardGameViewController ()
 
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (nonatomic) int flipCount;
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
+@property (strong, nonatomic) CardMatchingGame *game;
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
 @end
 
 @implementation CardGameViewController
 
-- (PlayingCardDeck *)deck
+- (CardMatchingGame *)game
 {
-    if(!_deck)
-        _deck = [[PlayingCardDeck alloc] init];
-        
-    return _deck;
+    if(!_game)
+        _game = [[CardMatchingGame alloc] initWithCardCount:[self.cardButtons count]
+                                                  usingDeck:[[PlayingCardDeck alloc] init]];
+    
+    return _game;
+}
+
+- (void)setCardButtons:(NSArray *)cardButtons
+{
+    _cardButtons = cardButtons;
+    [self updateUI];
 }
 
 - (void)setFlipCount:(int)flipCount
@@ -33,16 +44,24 @@
 
 - (IBAction)flipCard:(UIButton *)sender
 {
-    sender.selected = !sender.isSelected;
-    
-    //todo: perhaps check if the class is infact a PlayingCard before casting it?
-    //[myObject isKindOfClass:[NSString class]]
-    PlayingCard *card = (PlayingCard *)[self.deck drawRandomCard];
-    
-    [sender setTitle:card.contents forState:UIControlStateSelected];
-    
+    [self.game flipCardAtIndex:[self.cardButtons indexOfObject:sender]];
     self.flipCount++;
+    [self updateUI];
 }
 
+- (void)updateUI
+{
+    for(UIButton *cardButton in self.cardButtons){
+        Card *card = [self.game cardAtIndex:[self.cardButtons indexOfObject:cardButton]];
+        [cardButton setTitle:card.contents forState:UIControlStateSelected];
+        [cardButton setTitle:card.contents forState:UIControlStateSelected|UIControlStateDisabled];
+        cardButton.selected = card.isFaceUp;
+        cardButton.enabled = !card.isUnplayable;
+        
+        cardButton.alpha = (card.isUnplayable ? 0.3 : 1.0);
+        
+        self.scoreLabel.text = [NSString stringWithFormat:@"Score: %d", self.game.score];
+    }
+}
 
 @end
---
+++ Matchismo/Model/CardMatchingGame.h
@@ -0,0 +1,24 @@
+//
+//  CardMatchingGame.h
+//  Matchismo
+//
+//  Created by Kevin Mitchell on 7/2/13.
+//  Copyright (c) 2013 Kevin Mitchell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Card.h"
+#import "Deck.h"
+
+@interface CardMatchingGame : NSObject
+
+//Designated initializer
+- (id)initWithCardCount:(NSUInteger)count usingDeck:(Deck *)deck;
+
+- (void)flipCardAtIndex:(NSUInteger) index;
+- (Card *)cardAtIndex:(NSUInteger) index;
+
+@property (readonly, nonatomic) int score;
+
+
+@end
